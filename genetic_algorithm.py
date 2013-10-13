ba09fa99d20710883c86285a9d96f3d52813b8e8
alex141108 <alex1412108@gmail.com>
2013-10-13 12:53:39 +0100

    finished initialization function

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,9 +3,25 @@ import pprint
 random.seed
 pp=pprint.PrettyPrinter(indent=4)
 
-
-for i in range(0,1):
-	
-	for j in range(0,5):
+pool_gene={}
 
 
+def main():
+	global pool_gene
+	#pool_size=input("enter the pool size\n")
+	#length_gene=input("enter the gene length\n")
+	initialize_gene_pool()#pool_size, length_gene)
+	pp.pprint(pool_gene)
+
+def initialize_gene_pool(pool_size=4, length_gene=3):
+	for member in range(0,pool_size):
+		value_gene={}
+		value_member={}
+		for gene in range(0,length_gene):
+			value_gene[gene]=random.randint(0,1)
+		value_fitness=sum(value_gene.values())
+		value_member["fitness"]=value_fitness
+		value_member["gene"]=value_gene
+		pool_gene[member]=value_member
+
+main()
