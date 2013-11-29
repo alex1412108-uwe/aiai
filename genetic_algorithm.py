6947d60dbf95dd8b0b7afc2a0595b8736e65a507
alex141108 <alex1412108@gmail.com>
2013-11-29 13:00:54 +0000

    finished roulette wheel selection
    also moved parents_number to the main function

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -11,12 +11,13 @@ def main():
 	global pool_gene
 	pool_size=10#input("enter the pool size\n")
 	gene_length=5#input("enter the gene length\n")
+	parents_number=pool_size
 	initialize_gene_pool(pool_size, gene_length)
 	pp.pprint(pool_gene)
 	print()
-	parents_gene=roulette_wheel_selection(pool_size, gene_length)
+	parents_gene=roulette_wheel_selection(pool_size, gene_length, parents_number)
 	pp.pprint(parents_gene)
-	#single_point_crossover(pool_size, gene_length, parents_gene)
+	#single_point_crossover(pool_size, gene_length, parents_gene, parents_number)
 
 
 
@@ -31,37 +32,27 @@ def initialize_gene_pool(pool_size=2, gene_length=2):
 		value_member["gene"]=value_gene
 		pool_gene[member]=value_member
 
-def roulette_wheel_selection(pool_size, gene_length):
+def roulette_wheel_selection(pool_size, gene_length, parents_number):
 	global pp
 	global pool_gene
 	parents_gene={}
-	parents_number=pool_size
 	current_fitness_total=sum_of_dict_elements(pool_gene, "fitness")
-	#i=0 #resets the parent counter
-	#parent=0
-	#while i<parents_number:
 	for parent in range(0,parents_number):
 		cutoff=random.randint(0,current_fitness_total)
-		j=0 #resets the fitness counter
-		member=0 #since member gets incremented by one before the while loop can exit
-		condition=True
-		while condition:
-			condition=j<cutoff
-			j=j+pool_gene[member]["fitness"] #increases the fitness counter by the current members fitness
-			member=member+1
-		#while j<cutoff:
-		#	member=member+1
-		#	j=j+pool_gene[member]["fitness"] #increases the fitness counter by the current members fitness
-		parents_gene[parent]=member
-		#parent=parent+1
-		#i=i+1
+		fitness_sum=0 #resets the fitness counter
+		for member in range(0,pool_size):
+			fitness_sum=fitness_sum+pool_gene[member]["fitness"] #increases the fitness counter by the current members fitness
+			if fitness_sum>=cutoff: 
+				parents_gene[parent]=member
+				break
 	return parents_gene
 '''
-def single_point_crossover(pool_size, gene_length, parents_gene):
+def single_point_crossover(pool_size, gene_length, parents_gene, parents_number):
 	global pp
 	global pool_gene
 	i=0
 	while i<pool_size:
+	for 
 		gene1_front=0
 		gene1_back=0
 		gene2_front=0
