31fdbdd6f027f25d534a85dd1a3c1f1c740c4657
alex1412108 <alex1412108@gmail.com>
2014-03-12 21:31:02 +0000

    create rule initialization function

--- data_miner_rulebased.py
+++ data_miner_rulebased.py
@@ -2,6 +2,9 @@
 
 import csv
 import random
+import re #regular expressions library
+
+random.seed
 
 def main():
 	data=list(datareader())
@@ -10,6 +13,9 @@ def main():
 	dataset2=data[17:]
 	print dataset1
 	print dataset2
+	#if re.match
+	print initialize_rule_set()
+
 
 def datareader():
 	with open('data1.txt', 'rb') as csvfile:
@@ -18,5 +24,21 @@ def datareader():
 	return datafromfile
 		#for row in datafromfile:
 			#print row
+
+def initialize_rule_set(rule_set_size=10, rule_length=5):
+    rule_set = {}
+    for member in range(0,rule_set_size):
+        value_rule = list(range(rule_length))
+        value_member = {}
+        for rule in range(0,rule_length):
+            value_rule[rule] = random.choice(['0','1','[01]'])
+        value_fitness = 0
+        value_member["rule"] = value_rule
+        value_member["fitness"] = value_fitness
+        rule_set[member] = value_member
+    return rule_set
+
+#def fitness_test():
+	#''.join()
 
 main()
