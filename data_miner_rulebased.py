7dade849d635d6cbda4ef7d5fcfddd52e1325d30
alex1412108 <alex1412108@gmail.com>
2014-03-17 10:39:24 +0000

    complete fitness test function

--- data_miner_rulebased.py
+++ data_miner_rulebased.py
@@ -1,4 +1,3 @@
-
 #see march 3, 2014 lecture for more details
 #needs to learn from the first half to solve for the second half
 #input(first column) needs to result in the output (second column)
@@ -6,18 +5,22 @@
 import csv
 import random
 import re #regular expressions library
+import pprint
 
 random.seed
 
 def main():
-	data=list(datareader())
+	global pp
+	pp = pprint.PrettyPrinter(indent = 2)
+	data = list(datareader())
 	random.shuffle(data)
-	dataset1=data[:16]
-	dataset2=data[17:]
-	print dataset1
-	print dataset2
-	#if re.match
-	print initialize_rule_set()
+	dataset1 = data[:16]
+	dataset2 = data[17:]
+
+	rule_set = initialize_rule_set()
+	pp.pprint(rule_set)
+	rule_set = fitness_test(rule_set, dataset1)
+	pp.pprint(rule_set)
 
 
 def datareader():
@@ -28,7 +31,7 @@ def datareader():
 		#for row in datafromfile:
 			#print row
 
-def initialize_rule_set(rule_set_size=10, rule_length=5):
+def initialize_rule_set(rule_set_size=16, rule_length=5):
     rule_set = {}
     for member in range(0,rule_set_size):
         value_rule = list(range(rule_length))
@@ -37,11 +40,21 @@ def initialize_rule_set(rule_set_size=10, rule_length=5):
             value_rule[rule] = random.choice(['0','1','[01]'])
         value_fitness = 0
         value_member["rule"] = value_rule
+        value_member["result"] = random.choice([0,1])
         value_member["fitness"] = value_fitness
         rule_set[member] = value_member
     return rule_set
 
-#def fitness_test():
-	#''.join()
+def fitness_test(rule_set, dataset):
+	for key, member in rule_set.items():
+		fitness = 0
+		rule_string = ''.join(member['rule'])
+		for data in dataset:
+			if re.match(rule_string, data[0]):
+				fitness += 1
+				if member['result'] == data[1]:
+					fitness += 1
+		rule_set[key]['fitness'] = fitness
+	return rule_set
 
 main()
